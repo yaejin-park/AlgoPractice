f8f346dcdee55c0c0755f5e1ad80fb0596699a18
박예진 <80744051+yaejin-park@users.noreply.github.com>
2022-10-26 12:26:59 +0900

    [level 2] Title: 피보나치 수, Time: 3.13 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int[] dp;
+    
+    public int solution(int n) {
+        dp = new int[n+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]%1234567 + dp[i-2]%1234567;
+        }
+        
+        return dp[n]%1234567;
+    }
+    
+}
